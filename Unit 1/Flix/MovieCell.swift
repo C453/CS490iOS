bd02c4288112e276ef58a2491a3886e90d2da75f
Case Wright <wrigh404@purdue.edu>
2019-01-22 18:43:25 -0500

    Add unit 1

---
+++ Unit 1/Flix/MovieCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Case Wright on 1/14/19.
+//  Copyright © 2019 Case Wright. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
